13ae16cb35e5ad5ddc70afc4348eb000e0012191
Ivan Derevyannikov <vanyatok@gmail.com>
2022-09-04 20:07:12 +0700

    Add font and blue texture

--- CMakevcpkgSfmlProject/CMakevcpkgSfmlProject.cpp
+++ CMakevcpkgSfmlProject/CMakevcpkgSfmlProject.cpp
@@ -1,12 +1,25 @@
-<<<<<<< HEAD
-﻿// CMakevcpkgSfmlProject.cpp: определяет точку входа для приложения.
-//
-
 #include "CMakevcpkgSfmlProject.h"
 #include "AllClasses.h"
 
 int main()
 {
+	sf::Text text;
+	sf::Font font;
+	font.loadFromFile("C:/Users/vanya/Downloads/ComicMono.ttf");
+	// select the font
+	text.setFont(font); // font is a sf::Font
+
+	// set the string to display
+
+	// set the character size
+	text.setCharacterSize(24); // in pixels, not points!
+
+	// set the color
+	text.setFillColor(sf::Color(0,0,0));
+
+
+	std::string str;
+
 	sf::RenderWindow window(sf::VideoMode(1000, 500), "My test window");
 
 
@@ -18,19 +31,6 @@ int main()
 	sf::Texture bgTex;
 	bgTex.loadFromFile("C:/Users/vanya/Desktop/SFML programs/CMakevcpkgSfmlProject2/CMakevcpkgSfmlProject/white2.png");
 
-	sf::Sprite bgSpr;
-	bgSpr.setTexture(bgTex);
-	bgSpr.setPosition(50, 50);
-	bgSpr.setScale(0.1, 0.1);
-
-	sf::RenderTexture bgRendTex;
-	bgRendTex.create(500, 500);
-	bgRendTex.draw(bgSpr);
-	bgRendTex.display();
-
-	sf::Sprite bgSpr2;
-	bgSpr2.setTexture(bgRendTex.getTexture());
-
 	sf::Clock clock;
 	sf::Clock snakeClock;
 	sf::Clock animationClock;
@@ -41,7 +41,7 @@ int main()
 
 
 	std::vector<int> freeBlock((1000 / 50) * (500 / 50));
-	std::fill(freeBlock.begin(), freeBlock.end(), 1);
+	std::fill(freeBlock.begin()+20, freeBlock.end(), 1);
 
 	bool ispaused = false;
 
@@ -83,14 +83,21 @@ int main()
 
 			if (mySnake.checkRepeat()) {
 				mySnake.stopMove();
+				continue;
 			}
 
+			str = "Point: " + std::to_string(mySnake.getSnakeSize()-4);
+			text.setString(str);
 
-			window.clear();
-			window.draw(bgSpr2);
+			text.setPosition(500 - text.getLocalBounds().width, 0);
+
+			window.clear(sf::Color(0,141,150));
+			bg.draw(window);
 			mySnake.drawSnake(window);
 			food.draw(window);
 
+
+			window.draw(text);
 			window.display();
 
 		}
@@ -100,110 +107,3 @@ int main()
 	
 	
 }
-
-
-=======
-﻿// CMakevcpkgSfmlProject.cpp: определяет точку входа для приложения.
-//
-
-#include "CMakevcpkgSfmlProject.h"
-#include "AllClasses.h"
-
-int main()
-{
-	sf::RenderWindow window(sf::VideoMode(1000, 500), "My test window");
-
-
-	std::string filename = "C:/Users/vanya/Desktop/SFML programs/CMakevcpkgSfmlProject2/CMakevcpkgSfmlProject/FoodSprite.png";
-	Snake mySnake=Snake::Snake("C:/Users/vanya/Desktop/SFML programs/CMakevcpkgSfmlProject2/CMakevcpkgSfmlProject/Snake sprite sheet.png", 0, 0, 42, 42);
-	Food food(filename, 83, 94, 210, 210);
-
-
-	sf::Texture bgTex;
-	bgTex.loadFromFile("C:/Users/vanya/Desktop/SFML programs/CMakevcpkgSfmlProject2/CMakevcpkgSfmlProject/white2.png");
-
-	sf::Sprite bgSpr;
-	bgSpr.setTexture(bgTex);
-	bgSpr.setPosition(50, 50);
-	bgSpr.setScale(0.1, 0.1);
-
-	sf::RenderTexture bgRendTex;
-	bgRendTex.create(500, 500);
-	bgRendTex.draw(bgSpr);
-	bgRendTex.display();
-
-	sf::Sprite bgSpr2;
-	bgSpr2.setTexture(bgRendTex.getTexture());
-
-	sf::Clock clock;
-	sf::Clock snakeClock;
-	sf::Clock animationClock;
-	float CurrentFrame = 0;
-	float dx = 0;
-	float dy = 0;
-	float animatonParam = 0.001;
-
-
-	std::vector<int> freeBlock((1000 / 50) * (500 / 50));
-	std::fill(freeBlock.begin(), freeBlock.end(), 1);
-
-	bool ispaused = false;
-
-	while (window.isOpen()) {
-
-		float time = clock.getElapsedTime().asMicroseconds();
-		float snakeTime = snakeClock.getElapsedTime().asSeconds();
-		float animationTime = animationClock.getElapsedTime().asSeconds();
-
-		sf::Event event;
-		clock.restart();
-		time = time / 800;
-
-		
-		if (window.pollEvent(event)) {
-			if (event.type == sf::Event::Closed)
-				window.close();
-
-			else if (event.type ==sf::Event::KeyPressed) {
-				if (event.key.code == sf::Keyboard::P) {
-					ispaused = !ispaused;
-				}
-
-				else if ((mySnake.checkRepeat() || ispaused) && event.key.code == sf::Keyboard::R) {
-					ispaused = 0;
-					food.installFood(mySnake, freeBlock);
-					mySnake.setFirstPos();
-				}
-
-			}
-		}
-		
-		if (!ispaused) {
-			mySnake.pressKey(animationClock);
-			mySnake.move(animationClock, animationTime);
-
-
-			food.installFood(mySnake, freeBlock);
-
-			if (mySnake.checkRepeat()) {
-				mySnake.stopMove();
-			}
-
-
-			window.clear();
-			window.draw(bgSpr2);
-			mySnake.drawSnake(window);
-			food.draw(window);
-
-			window.display();
-
-		}
-	}
-
-	return 0;
-	
-	
-}
-
-
->>>>>>> 2c03db62ecbde5e524f738483ed066ef8fcd25c8
